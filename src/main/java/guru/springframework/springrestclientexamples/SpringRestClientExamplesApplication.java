15a02f689c1a4dcc0ee7425d8609b70701b1023f
John Thompson <john@springframework.guru>
2017-09-21 16:16:25 -0400

    initial project add

---
+++ src/main/java/guru/springframework/springrestclientexamples/SpringRestClientExamplesApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.springrestclientexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestClientExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestClientExamplesApplication.class, args);
+	}
+}
